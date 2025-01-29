50a49232b648aaf4b25669c384816c15b361f0a5
draconzy <129961852+draconzy@users.noreply.github.com>
2025-01-29 21:05:47 +0100

    Add files via upload

---
+++ mon1erproject.py
@@ -0,0 +1,18 @@
+def main():
+    # création d'une variable 'username' ayant pour valeur le mot enzo
+    username = "enzo"
+    username = "enzogoat"  # on change la valeur de username
+    # création d'une variable 'age' ayant pour valeur le mot 17
+    age = 17
+    # change la valeur de age avec 25
+    age = 25
+    # affiche la nouvelle valeur de age
+    age = age + 1
+    print("salut " + username + ", vous avez " + str (age) + "ans !")  # affiche le username et l'age
+
+
+if __name__ == '__main__':
+    main()  # appel de la fonction main()
+
+
+
